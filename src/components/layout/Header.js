dcad4245488060b5f7baa03a3b8b56506b99ec39
Marcys <moovspace@github.com>
2020-01-30 21:27:44 +0100

    ReactJS Router sample app

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Link, /* NavLink */ } from 'react-router-dom'
+import { /* Link, */ NavLink } from 'react-router-dom'
 
 // NavLink - Add active link style
-// <NavLink exact className="nav-link" activeStyle={{ color: "red" }} to="/"> Home </NavLink>
-// <NavLink className="nav-link" activeStyle={{ color: "red" }} to="/about"> About </NavLink>
+// <NavLink exact className="nav-link" activeClassName="nav-link-active" activeStyle={{ color: "red" }} to="/"> Home </NavLink>
+// <NavLink className="nav-link" activeClassName="nav-link-active" activeStyle={{ color: "red" }} to="/about"> About </NavLink>
+// Link
+// <Link exact className="nav-link" to="/"> Home </Link>
+// <Link className="nav-link" to="/about"> About </Link>
+// <Link className="nav-link" to="/post/777/image/888"> Post Id </Link>
 
 function Header() {
 	return (
@@ -11,9 +15,9 @@ function Header() {
 			<h1>My secret list</h1>
 
 			<div className="menu-top">
-				<Link exact className="nav-link" to="/"> Home </Link>
-				<Link className="nav-link" to="/about"> About </Link>
-				<Link className="nav-link" to="/post/777/image/888"> Post Id </Link>
+				<NavLink exact className="nav-link" activeClassName="nav-link-active" to="/"> Home </NavLink>
+				<NavLink className="nav-link" activeClassName="nav-link-active" to="/about"> About </NavLink>
+				<NavLink className="nav-link" activeClassName="nav-link-active" to="/post/777/image/888"> Post Id </NavLink>
 			</div>
 
 		</header>		
